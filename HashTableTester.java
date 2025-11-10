e2ec44fefd1c96e3a5ffc62fffb762cd4e386681
Max Barker <maxbarker26@gmail.com>
2025-11-10 10:42:33 -0900

    Add test for put method of hashtable

--- HashTableTester.java
+++ HashTableTester.java
@@ -2,13 +2,26 @@ package assign09;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class HashTableTester {
+	HashTable<String, Integer> booksSold;
+	@BeforeEach
+	void setUp() {
+		booksSold = new HashTable<String, Integer>();
+		booksSold.put("The Wind in the Willows", 50);
+		booksSold.put("Blood Meridian", 25);
+		booksSold.put("Children of Dune", 120);
+		booksSold.put("Frankenstein", 80);
+	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testPutStringKey() {
+		booksSold.put("Don Quixote", 40);
+		
+		assertEquals(5, booksSold.size());
+		assertTrue(booksSold.containsKey("Don Quixote"));
 	}
 
 }
